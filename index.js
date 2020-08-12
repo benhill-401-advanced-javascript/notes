eb903eacad9480fd8c62f371ef67b85319fcc665
BEN <benjamin.hill28@gmail.com>
2020-08-11 19:46:50 -0700

    minimist is taking input and giving output

--- index.js
+++ index.js
@@ -3,8 +3,12 @@
 // Requires the library files you will be writing (input, notes)
 const Input = require('./lib/input.js');
 
-const input = new Input();
 
 // Instantiates an instance of an “Input” parser module
 // Parses the command line input and returns the command and data
 // Passes the command to the Notes library, which executes the command
+
+
+
+
+
