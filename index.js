11d68b7fff83c44b02de0cc3bdf0d0abfa3e83a7
BEN <benjamin.hill28@gmail.com>
2020-08-12 00:21:31 -0700

    input is returning output the way expected

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 'use strict';
 
 // Requires the library files you will be writing (input, notes)
-const Input = require('./lib/input.js');
+const Input = require('./lib/input');
+const Notes = require('./lib/notes');
 
 
-// Instantiates an instance of an “Input” parser module
-// Parses the command line input and returns the command and data
-// Passes the command to the Notes library, which executes the command
+const input = new Input();
+const note = new Notes(input);
+
+if (input.validate()) {
+  note.execute();
+} else {
+  process.exit(9);
+}
 
 
+/*----------WHAT DO YOU WANT NOTESY TO DO?
+This app will be rolled out in four phases
+After phase one we will be adding features for delete and list
+For Phase one all i want it to do is have the user enter in node index.js --add "enter in a command" in the command line
+this will return a object in the command line when i press enter
+the process.argv is the way that will track my spaces
+the splice(2) is cutting out my node and index directory pathways
+minimist is taking the array that i'm getting back and turning into an objec that is looking for key: value pairs
+input.js is going to evaluate and validate the input from the user (--add and string that returns from input)
+notes.js is going to execute the action and command into index.js which renders in the CLI
 
+
+*/
 
 
