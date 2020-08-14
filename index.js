e03df7e3726564eabd72bd40ee138df172f7649d
BEN <benjamin.hill28@gmail.com>
2020-08-13 19:23:14 -0700

    refactored input.js and notes.js

--- index.js
+++ index.js
@@ -27,26 +27,4 @@ the splice(2) is cutting out my node and index directory pathways
 minimist is taking the array that i'm getting back and turning into an objec that is looking for key: value pairs
 input.js is going to evaluate and validate the input from the user (--add and string that returns from input)
 notes.js is going to execute the action and command into index.js which renders in the CLI
-
-
-// */
-// 'use strict'
-// // const minimist = require('minimist');
-// const Input = require('./lib/input');
-// // const Note = require('./lib/notes');
-// const Notes = require('./lib/notes');
-// const input = new Input();
-// const note = new Notes(input);
-// if (input.valid()) {
-//   note.execute()
-// } else {
-//   process.exit(9)
-// }
-//our notes application will accept and validate the user's input, and confirm that the note was valid.//
-// As a user, I want to be able to call the application using command line standard syntax, indicating the text of a note I wish to add so that the system will eventually be able to save this note.
-// As a user, I expect that the application will confirm my intent. 
-// console.log(minimist(process.argv.slice(2)));
-// // this is meant to restrict the amount of crap you get back. 
-//npm init -y initializes the package, and creates a package.json (metadata for handling the file)
-// minimist deals specifically with parsing command line stuff- returns an object instead of an array. Creates key value pairs out of your input.
-
+*/
